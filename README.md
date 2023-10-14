a2f170f80a5d0a6bd155e0293f07c6ec896598fd
Marcin Frydrych <marcinf23@gmail.com>
2023-10-14 02:53:14 +0200

    Upload finished project.

---
+++ README.md
@@ -0,0 +1,1 @@
+# WeSplit-iOS-17
---
+++ We Split for iOS 17/We_Split_for_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  We_Split_for_iOS_17App.swift
+//  We Split for iOS 17
+//
+//  Created by Marcin Frydrych on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct We_Split_for_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
